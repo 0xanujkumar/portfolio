f6375fef78657a749cfab45051952f6f9b15bce1
Anuj Kumar <anujkumar992862@gmail.com>
2024-04-25 20:13:52 +0530

    Done

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {FaGithub, FaLinkedin} from "react-icons/fa";
+import {GrMail} from "react-icons/gr";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h4>Developed by Anuj Kumar</h4>
+      <div className='footerLinks'>
+        <a href="https://github.com/0xanujkumar" target='_blank'><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/0xanujkumar" target='_blank'><FaLinkedin/></a>
+        <a href='mailTo:anujkumar992862@gmail.com' target='_blank'><GrMail/></a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
